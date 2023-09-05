b71af6c05e50410f735aeb70644109b9522ecaa2
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-09-05 10:17:47 -0700

    remove comments from dist

--- dist/reactive-object.d.ts
+++ dist/reactive-object.d.ts
@@ -1,15 +1,12 @@
 export declare class ReactiveObject<T> {
-  private store;
-  private effects;
-  constructor(obj: T, options?: Partial<EffectOptions>);
-  registerEffect(
-    effect: (data: any) => void,
-    type?: "get" | "set" | null
-  ): string;
-  removeEffect(hash: string, type?: "get" | "set" | null): void;
+    private store;
+    private effects;
+    constructor(obj: T, options?: Partial<EffectOptions>);
+    registerEffect(effect: (data: any) => void, type?: "get" | "set" | null): string;
+    removeEffect(hash: string, type?: "get" | "set" | null): void;
 }
 interface EffectOptions {
-  full: boolean;
-  all: boolean;
+    full: boolean;
+    all: boolean;
 }
 export {};
--- dist/reactive-object.js
+++ dist/reactive-object.js
@@ -1,76 +1,74 @@
 export class ReactiveObject {
-  store;
-  effects;
-  constructor(obj, options) {
-    const self = this;
-    this.store = obj;
-    this.effects = {
-      options,
-      getEffects: {},
-      setEffects: {},
-    };
-    const handler = {
-      get(target, key, receiver) {
-        if (target == self) return;
-        if (key === self.registerEffect.name) {
-          return function () {
-            return self.registerEffect.apply(self, arguments);
-          };
-        }
-        if (key === self.removeEffect.name) {
-          return function () {
-            return self.removeEffect.apply(self, arguments);
-          };
-        }
-        if (typeof target[key] === "object" && target[key] !== null) {
-          if (!target._isProxy) {
-            target._isProxy = true;
-            return new Proxy(target[key], handler);
-          }
-          return target[key];
-        } else {
-          for (const h in self.effects.getEffects) {
-            self.effects.getEffects[h].apply(self, [key]);
-          }
-          return target[key];
-        }
-      },
-      set(target, key, value) {
-        // prevent effect execution if same value; TODO: allow to be configurable
-        if (target[key] === value) return true;
-        if (key === Object.getPrototypeOf(self.effects).name) {
-          return false;
-        }
-        const old = target[key];
-        target[key] = value;
-        if (
-          key !== "_isProxy" &&
-          key !== self.registerEffect.name &&
-          key !== self.removeEffect.name
-        ) {
-          for (const h in self.effects.setEffects) {
-            self.effects.setEffects[h].apply(self, [key, value, old]);
-          }
-        }
-        return true;
-      },
-    };
-    const proxy = new Proxy(obj, handler);
-    return proxy;
-  }
-  registerEffect(effect, type) {
-    const effects =
-      type === "get" ? this.effects.getEffects : this.effects.setEffects;
-    let hash;
-    do {
-      hash = Math.random().toString(16).substring(2, 15);
-    } while (effects[hash]);
-    effects[hash] = effect;
-    return hash;
-  }
-  removeEffect(hash, type) {
-    const effects =
-      type === "get" ? this.effects.getEffects : this.effects.setEffects;
-    delete effects[hash];
-  }
+    store;
+    effects;
+    constructor(obj, options) {
+        const self = this;
+        this.store = obj;
+        this.effects = {
+            options,
+            getEffects: {},
+            setEffects: {},
+        };
+        const handler = {
+            get(target, key, receiver) {
+                if (target == self)
+                    return;
+                if (key === self.registerEffect.name) {
+                    return function () {
+                        return self.registerEffect.apply(self, arguments);
+                    };
+                }
+                if (key === self.removeEffect.name) {
+                    return function () {
+                        return self.removeEffect.apply(self, arguments);
+                    };
+                }
+                if (typeof target[key] === "object" && target[key] !== null) {
+                    if (!target._isProxy) {
+                        target._isProxy = true;
+                        return new Proxy(target[key], handler);
+                    }
+                    return target[key];
+                }
+                else {
+                    for (const h in self.effects.getEffects) {
+                        self.effects.getEffects[h].apply(self, [key]);
+                    }
+                    return target[key];
+                }
+            },
+            set(target, key, value) {
+                if (target[key] === value)
+                    return true;
+                if (key === Object.getPrototypeOf(self.effects).name) {
+                    return false;
+                }
+                const old = target[key];
+                target[key] = value;
+                if (key !== "_isProxy" &&
+                    key !== self.registerEffect.name &&
+                    key !== self.removeEffect.name) {
+                    for (const h in self.effects.setEffects) {
+                        self.effects.setEffects[h].apply(self, [key, value, old]);
+                    }
+                }
+                return true;
+            },
+        };
+        const proxy = new Proxy(obj, handler);
+        return proxy;
+    }
+    registerEffect(effect, type) {
+        const effects = type === "get" ? this.effects.getEffects : this.effects.setEffects;
+        let hash;
+        do {
+            hash = Math.random().toString(16).substring(2, 15);
+        } while (effects[hash]);
+        effects[hash] = effect;
+        return hash;
+    }
+    removeEffect(hash, type) {
+        const effects = type === "get" ? this.effects.getEffects : this.effects.setEffects;
+        delete effects[hash];
+    }
 }
