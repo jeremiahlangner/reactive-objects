d1bca893d2ac169cceb430b5c84759996bb7b7c4
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-09-08 12:38:43 -0700

    add type annotations to package

--- dist/reactive-object.d.ts
+++ dist/reactive-object.d.ts
@@ -1,30 +1,27 @@
 export declare class ReactiveObject<T> {
-  private effects;
-  constructor(obj: T, options?: Partial<EffectOptions>);
-  registerEffect(
-    effect: (data: any) => void,
-    type?: "get" | "set" | null
-  ): string;
-  removeEffect(hash: string, type?: "get" | "set" | null): void;
+    private effects;
+    constructor(obj: T, options?: Partial<EffectOptions>);
+    registerEffect(effect: (data: any) => void, type?: "get" | "set" | null): string;
+    removeEffect(hash: string, type?: "get" | "set" | null): void;
 }
 export interface EffectOptions {
-  full: boolean;
-  all: boolean;
+    full: boolean;
+    all: boolean;
 }
 export interface GetEffectArguments {
-  key: string;
+    key: string;
 }
 export interface SetEffectArguments {
-  key: string;
-  value: any;
-  old: any;
+    key: string;
+    value: any;
+    old: any;
 }
 export interface Effects {
-  options: Partial<EffectOptions>;
-  getEffects: {
-    [hash: string]: (args: GetEffectArguments) => void;
-  };
-  setEffects: {
-    [hash: string]: (args: SetEffectArguments) => void;
-  };
+    options: Partial<EffectOptions>;
+    getEffects: {
+        [hash: string]: (args: GetEffectArguments) => void;
+    };
+    setEffects: {
+        [hash: string]: (args: SetEffectArguments) => void;
+    };
 }
